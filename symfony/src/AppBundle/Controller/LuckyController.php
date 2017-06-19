c06e7040a986b709270410c80ad600e08b07b6c4
KatyaYatsenco <k.yatsenco@gmail.com>
2017-06-19 20:21:30 +0300

    Updated old data.

--- symfony/src/AppBundle/Controller/LuckyController.php
+++ symfony/src/AppBundle/Controller/LuckyController.php
@@ -3,11 +3,14 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Cache\Exception;
+use Symfony\Component\Validator\Mapping\Cache\CacheInterface;
+use Symfony\Component\Cache\Exception\CacheException;
 
 class LuckyController
 {
     /**
-     * @Route("/lucky/number")
+     * @Route("/")
      */
     public function numberAction()
     {
